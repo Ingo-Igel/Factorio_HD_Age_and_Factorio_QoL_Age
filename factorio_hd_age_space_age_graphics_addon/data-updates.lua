bc2719b9c59b8ad1664d33b03ef888b0f02f1214
Ingo_Igel <Ingo_Igel@noreply.codeberg.org>
2025-08-17 17:14:21 +0200

    added PlanetsLib compatibility

--- factorio_hd_age_space_age_graphics_addon/data-updates.lua
+++ factorio_hd_age_space_age_graphics_addon/data-updates.lua
@@ -1,3 +1,71 @@
-if not mods["PlanetsLib"] then
-	require("starmap")
+if mods["PlanetsLib"] then
+	PlanetsLib:extend({
+		{
+			type = "space-location",
+			name = "shattered-planet_Big",
+			orbit = {
+				parent = {
+					type = "space-location",
+					name = "shattered-planet"
+				},
+				distance = 0,
+				orientation = 0,
+				sprite = {
+					type = "sprite",
+					filename = "__factorio_hd_age_space_age_graphics_addon__/graphics/shattered-planet.png",
+					size = 960,
+					scale = 0.4,
+				}
+			},
+			sprite_only = true
+		},
+			{
+			type = "space-location",
+			name = "asteroid_belt_1",
+			orbit = {
+				parent = {
+					type = "space-location",
+					name = "star"
+				},
+				distance = 0,
+				orientation = 0,
+				sprite = {
+					type = "sprite",
+					filename = "__custom-space-map__/graphics/starmap/Starmap_Solar_System_05.png",
+					size = 2436,
+					scale = 1,
+				}
+			},
+			sprite_only = true
+		},
+			{
+			type = "space-location",
+			name = "asteroid_belt_2",
+			orbit = {
+				parent = {
+					type = "space-location",
+					name = "star"
+				},
+				distance = 0,
+				orientation = 0,
+				sprite = {
+					type = "sprite",
+					filename = "__custom-space-map__/graphics/starmap/Starmap_Solar_System_06.png",
+					size = 2436,
+					scale = 1.5
+				}
+			},
+			sprite_only = true
+		}
+	})
+
+else
+	data.raw["utility-sprites"]["default"]["starmap_star"].layers[6].scale = 1
+	data.raw["utility-sprites"]["default"]["starmap_star"].layers[7].scale = 1.5
+	data.raw["utility-sprites"]["default"]["starmap_star"].layers[8] = {
+		filename = "__factorio_hd_age_space_age_graphics_addon__/graphics/shattered-planet.png",
+		size = 960,
+		scale = 0.4,
+		shift = { 2565, 10 }
+	}
 end
