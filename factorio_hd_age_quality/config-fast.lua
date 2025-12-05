cc7326de2445cc800ac65756b890bb4166c534fa
Ingo_Igel <Ingo_Igel@noreply.codeberg.org>
2025-12-06 00:23:57 +0100

    added fast load

---
+++ factorio_hd_age_quality/config-fast.lua
@@ -0,0 +1,34 @@
+local exclude = {	"*shadow*",
+					"*smoke*"
+				}
+
+local str= tostring(settings.startup["f_hd_a_q_disable_string"].value)
+if str then
+	for word in string.gmatch(str,'[^,%s]+') do
+		table.insert (exclude, "*"..word.."*")
+	end
+end
+
+if mods["factorio_hd_age_modpack"] or mods["factorio_hd_age_modpack_optional"] or mods["factorio_hd_age_modpack_base_game_only"] or mods["factorio_hd_age_modpack_base_game_optional"] then
+	local str1= tostring(settings.startup["f_hd_a_disable_string"].value)
+	if str1 then
+		for word in string.gmatch(str1,'[^,%s]+') do
+			table.insert (exclude, "*"..word.."*")
+		end
+	end
+end
+
+return {
+	scalefactor = 2,
+	resource_pack_name = "factorio_hd_age_quality",
+	exclude_names = exclude,
+	data = {
+		quality = {
+			graphics = {
+				entity = {
+					["recycler"]={}
+				}
+			}
+		}
+	}
+}
