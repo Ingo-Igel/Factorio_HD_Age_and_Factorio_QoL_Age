b270cf1a3ce51ac8ef02c5c3db6e15482dc7e1f7
Ingo_Igel <Ingo_Igel@noreply.codeberg.org>
2025-12-05 12:10:25 +0100

    added loading methode option and strukture

--- factorio_hd_age_quality/settings.lua
+++ factorio_hd_age_quality/settings.lua
@@ -5,5 +5,12 @@ data:extend{
 		name = "f_hd_a_q_disable_string",
 		default_value = "",
 		allow_blank = true,
+	},
+	{
+		type = "string-setting",
+		setting_type = "startup",
+		name = "f_hd_a_q_loading_methode",
+		default_value = "fallback",
+		allowed_values = {"fast", "fallback"}
 	}
 }
