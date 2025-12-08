6c52137fe19d08160ad010c32ccc68dd0dc97e81
Ingo_Igel <Ingo_Igel@noreply.codeberg.org>
2025-12-08 20:11:49 +0100

    code cleanup

--- factorio_hd_age_base_game_base/prototypes/rocket-silo.lua
+++ factorio_hd_age_base_game_base/prototypes/rocket-silo.lua
@@ -1,9 +1,10 @@
 if not settings.startup["f_hd_a_bg_b_disable_arms_right"].value then
 	data.raw["rocket-silo"]["rocket-silo"].arm_02_right_animation = {
-		filename = "__factorio_hd_age_base_game_base__/data/base/graphics/entity/rocket-silo/08-rocket-silo-arms-right.png",
+		filename =
+		"__factorio_hd_age_base_game_base__/data/base/graphics/entity/rocket-silo/08-rocket-silo-arms-right.png",
 		priority = "medium",
-		width = 182*2,
-		height = 188*2,
+		width = 182 * 2,
+		height = 188 * 2,
 		frame_count = 32,
 		line_length = 16,
 		animation_speed = 0.3,
