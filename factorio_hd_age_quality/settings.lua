73707bbe25b70e07ad18d93f6c66f9889c517b05
Ingo_Igel <Ingo_Igel@noreply.codeberg.org>
2025-12-08 18:53:57 +0100

    changed new faster loading methode to only methode available

--- factorio_hd_age_quality/settings.lua
+++ factorio_hd_age_quality/settings.lua
@@ -1,16 +1,9 @@
-data:extend{
+data:extend {
 	{
 		type = "string-setting",
 		setting_type = "startup",
 		name = "f_hd_a_q_disable_string",
 		default_value = "",
 		allow_blank = true,
-	},
-	{
-		type = "string-setting",
-		setting_type = "startup",
-		name = "f_hd_a_q_loading_methode",
-		default_value = "fallback",
-		allowed_values = {"fast", "fallback"}
 	}
 }
