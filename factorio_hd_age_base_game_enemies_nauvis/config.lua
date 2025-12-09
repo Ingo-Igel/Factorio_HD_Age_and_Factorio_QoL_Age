871e5760d8206e25cceaf64796aabb4cdc2f0fee
Ingo_Igel <Ingo_Igel@noreply.codeberg.org>
2025-12-09 18:08:26 +0100

    added new faster loading methode

--- factorio_hd_age_base_game_enemies_nauvis/config.lua
+++ factorio_hd_age_base_game_enemies_nauvis/config.lua
@@ -1,41 +1,40 @@
-local exclude = {	"*shadow*",
-					"*reflection*",
-					"*visualisation*",
-					"*worm-attack*",
-					"*biter-*"
-				}
+local exclude = { "*shadow*",
+	"*reflection*",
+	"*visualisation*",
+	"*worm-attack*",
+	"*biter-*"
+}
 
-local str= tostring(settings.startup["f_hd_a_bg_en_disable_string"].value)
+local str = tostring(settings.startup["f_hd_a_bg_en_disable_string"].value)
 if str then
-	for word in string.gmatch(str,'[^,%s]+') do
-		table.insert (exclude, "*"..word.."*")
+	for word in string.gmatch(str, '[^,%s]+') do
+		table.insert(exclude, "*" .. word .. "*")
 	end
 end
 
 if mods["factorio_hd_age_modpack"] or mods["factorio_hd_age_modpack_optional"] or mods["factorio_hd_age_modpack_base_game_only"] or mods["factorio_hd_age_modpack_base_game_optional"] then
-	local str1= tostring(settings.startup["f_hd_a_disable_string"].value)
+	local str1 = tostring(settings.startup["f_hd_a_disable_string"].value)
 	if str1 then
-		for word in string.gmatch(str1,'[^,%s]+') do
-			table.insert (exclude, "*"..word.."*")
+		for word in string.gmatch(str1, '[^,%s]+') do
+			table.insert(exclude, "*" .. word .. "*")
 		end
 	end
 end
 
 return {
+	scalefactor = 2,
 	resource_pack_name = "factorio_hd_age_base_game_enemies_nauvis",
+	exclude_names = exclude,
+	white_list = "__base__/graphics/entity/",
 	data = {
-		__settings__ = {
-			exclude_names = exclude,
-			upscale = 2
-		},
 		base = {
 			graphics = {
 				entity = {
-					["acid-splash"]={},
-					["acid-sticker"]={},
-					["biter"]={},
-					["spawner"]={},
-					["worm"]={}
+					["acid-splash"] = {},
+					["acid-sticker"] = {},
+					["biter"] = {},
+					["spawner"] = {},
+					["worm"] = {}
 				}
 			}
 		}
