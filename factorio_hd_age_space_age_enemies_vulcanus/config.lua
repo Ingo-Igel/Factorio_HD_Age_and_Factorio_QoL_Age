5120df2998df107b1f64bcadcb1726af79d11ee3
Ingo_Igel <Ingo_Igel@noreply.codeberg.org>
2025-12-10 14:42:32 +0100

    added new faster loading methode

--- factorio_hd_age_space_age_enemies_vulcanus/config.lua
+++ factorio_hd_age_space_age_enemies_vulcanus/config.lua
@@ -1,36 +1,35 @@
 local exclude = {}
 
-local str= tostring(settings.startup["f_hd_a_sa_ev_disable_string"].value)
+local str = tostring(settings.startup["f_hd_a_sa_ev_disable_string"].value)
 if str then
-	for word in string.gmatch(str,'[^,%s]+') do
-		table.insert (exclude, "*"..word.."*")
+	for word in string.gmatch(str, '[^,%s]+') do
+		table.insert(exclude, "*" .. word .. "*")
 	end
 end
 
 if mods["factorio_hd_age_modpack"] or mods["factorio_hd_age_modpack_optional"] or mods["factorio_hd_age_modpack_base_game_only"] or mods["factorio_hd_age_modpack_base_game_optional"] then
-	local str1= tostring(settings.startup["f_hd_a_disable_string"].value)
+	local str1 = tostring(settings.startup["f_hd_a_disable_string"].value)
 	if str1 then
-		for word in string.gmatch(str1,'[^,%s]+') do
-			table.insert (exclude, "*"..word.."*")
+		for word in string.gmatch(str1, '[^,%s]+') do
+			table.insert(exclude, "*" .. word .. "*")
 		end
 	end
 end
 
 return {
+	scalefactor = 2,
 	resource_pack_name = "factorio_hd_age_space_age_enemies_vulcanus",
+	exclude_names = exclude,
+	white_list = "__space-age__/graphics/entity/demolisher/",
 	data = {
-		__settings__ = {
-			exclude_names = exclude,
-			upscale = 2
-		},
-		["space-age"]={
+		["space-age"] = {
 			graphics = {
 				entity = {
-					["demolisher"]={
-						["fissure"]={},
-						["magma-eruption"]={},
-						["trail-lower"]={},
-						["trail-upper"]={}
+					["demolisher"] = {
+						["fissure"] = {},
+						["magma-eruption"] = {},
+						["trail-lower"] = {},
+						["trail-upper"] = {}
 					}
 				}
 			}
