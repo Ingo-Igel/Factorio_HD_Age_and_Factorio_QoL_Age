e8d553771a09ed3ad80ce12ea73f8e7907b9256d
Ingo_Igel <Ingo_Igel@noreply.codeberg.org>
2025-12-10 14:51:56 +0100

    added new faster loading methode

--- factorio_hd_age_space_age_logistics/config.lua
+++ factorio_hd_age_space_age_logistics/config.lua
@@ -1,50 +1,50 @@
-local exclude = {	"*shadow*",
-					"*reflections*"
-				}
+local exclude = {
+	"*shadow*",
+	"*reflections*"
+}
 
-local str= tostring(settings.startup["f_hd_a_sa_l_disable_string"].value)
+local str = tostring(settings.startup["f_hd_a_sa_l_disable_string"].value)
 if str then
-	for word in string.gmatch(str,'[^,%s]+') do
-		table.insert (exclude, "*"..word.."*")
+	for word in string.gmatch(str, '[^,%s]+') do
+		table.insert(exclude, "*" .. word .. "*")
 	end
 end
 
 if mods["factorio_hd_age_modpack"] or mods["factorio_hd_age_modpack_optional"] or mods["factorio_hd_age_modpack_base_game_only"] or mods["factorio_hd_age_modpack_base_game_optional"] then
-	local str1= tostring(settings.startup["f_hd_a_disable_string"].value)
+	local str1 = tostring(settings.startup["f_hd_a_disable_string"].value)
 	if str1 then
-		for word in string.gmatch(str1,'[^,%s]+') do
-			table.insert (exclude, "*"..word.."*")
+		for word in string.gmatch(str1, '[^,%s]+') do
+			table.insert(exclude, "*" .. word .. "*")
 		end
 	end
 end
 
 return {
+	scalefactor = 2,
 	resource_pack_name = "factorio_hd_age_space_age_logistics",
+	exclude_names = exclude,
+	white_list = "__space-age__/graphics/entity/",
 	data = {
-		__settings__ = {
-			exclude_names = exclude,
-			upscale = 2
-		},
-		["space-age"]={
+		["space-age"] = {
 			graphics = {
 				entity = {
-					["cargo-hubs"]={
-						["bays"]={},
-						["connections"]={},
-						["hatches"]={},
-						["hubs"]={}
+					["cargo-hubs"] = {
+						["bays"] = {},
+						["connections"] = {},
+						["hatches"] = {},
+						["hubs"] = {}
 					},
-					["cargo-pod"]={},
-					["stack-inserter"]={ ["*"]={}, ["remnants"]={} },
-					["thruster"]={
-						["thruster-bckg*"]={},
-						["thruster-pipe-connection*"]={},
-						["thruster-remnants*"]={},
-						["thruster-smoke*"]={}
+					["cargo-pod"] = {},
+					["stack-inserter"] = { ["*"] = {}, ["remnants"] = {} },
+					["thruster"] = {
+						["thruster-bckg*"] = {},
+						["thruster-pipe-connection*"] = {},
+						["thruster-remnants*"] = {},
+						["thruster-smoke*"] = {}
 					},
-					["turbo-splitter"]={ ["*"]={}, ["remnants"]={} },
-					["turbo-transport-belt"]={ ["remnants"]={} },
-					["turbo-underground-belt"]={ ["*"]={}, ["remnants"]={} }
+					["turbo-splitter"] = { ["*"] = {}, ["remnants"] = {} },
+					["turbo-transport-belt"] = { ["remnants"] = {} },
+					["turbo-underground-belt"] = { ["*"] = {}, ["remnants"] = {} }
 				}
 			}
 		}
