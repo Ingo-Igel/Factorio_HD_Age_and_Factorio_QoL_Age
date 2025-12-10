4a3d0712a57f48066a4d81076b016bcc022b0de9
Ingo_Igel <Ingo_Igel@noreply.codeberg.org>
2025-12-10 15:41:22 +0100

    added new faster loading methode

--- factorio_hd_age_space_age_terrain_gleba/config.lua
+++ factorio_hd_age_space_age_terrain_gleba/config.lua
@@ -1,44 +1,43 @@
 local exclude = {}
 
-local str= tostring(settings.startup["f_hd_a_sa_tg_disable_string"].value)
+local str = tostring(settings.startup["f_hd_a_sa_tg_disable_string"].value)
 if str then
-	for word in string.gmatch(str,'[^,%s]+') do
-		table.insert (exclude, "*"..word.."*")
+	for word in string.gmatch(str, '[^,%s]+') do
+		table.insert(exclude, "*" .. word .. "*")
 	end
 end
 
 if mods["factorio_hd_age_modpack"] or mods["factorio_hd_age_modpack_optional"] or mods["factorio_hd_age_modpack_base_game_only"] or mods["factorio_hd_age_modpack_base_game_optional"] then
-	local str1= tostring(settings.startup["f_hd_a_disable_string"].value)
+	local str1 = tostring(settings.startup["f_hd_a_disable_string"].value)
 	if str1 then
-		for word in string.gmatch(str1,'[^,%s]+') do
-			table.insert (exclude, "*"..word.."*")
+		for word in string.gmatch(str1, '[^,%s]+') do
+			table.insert(exclude, "*" .. word .. "*")
 		end
 	end
 end
 
 return {
+	scalefactor = 2,
 	resource_pack_name = "factorio_hd_age_space_age_terrain_gleba",
+	exclude_names = exclude,
+	white_list = "__space-age__/graphics/terrain/",
 	data = {
-		__settings__ = {
-			exclude_names = exclude,
-			upscale = 2
-		},
-		["space-age"]={
+		["space-age"] = {
 			graphics = {
 				terrain = {
-					["cliffs"]={
-						["gleba"]={
-							["cliff-gleba-entrance-lower.png"]={},
-							["cliff-gleba-inner-lower.png"]={},
-							["cliff-gleba-outer-lower.png"]={},
-							["cliff-gleba-sides-lower.png"]={}
+					["cliffs"] = {
+						["gleba"] = {
+							["cliff-gleba-entrance-lower.png"] = {},
+							["cliff-gleba-inner-lower.png"] = {},
+							["cliff-gleba-outer-lower.png"] = {},
+							["cliff-gleba-sides-lower.png"] = {}
 						},
 					},
-					["gleba"]={
-						["wetland-dead-skin-shader.png"]={},
-						["wetland-green-slime-shader.png"]={},
-						["wetland-pink-tentacle-shader.png"]={},
-						["wetland-purple-tentacle-shader.png"]={}
+					["gleba"] = {
+						["wetland-dead-skin-shader.png"] = {},
+						["wetland-green-slime-shader.png"] = {},
+						["wetland-pink-tentacle-shader.png"] = {},
+						["wetland-purple-tentacle-shader.png"] = {}
 					}
 				}
 			}
