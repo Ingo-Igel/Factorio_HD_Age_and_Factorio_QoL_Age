4876edaa3e3e1d31502f6a89d4bbed38e119c59a
Ingo_Igel <Ingo_Igel@noreply.codeberg.org>
2025-12-10 14:53:25 +0100

    code cleanup

--- factorio_hd_age_space_age_logistics/prototypes/turbo-transport-belt.lua
+++ factorio_hd_age_space_age_logistics/prototypes/turbo-transport-belt.lua
@@ -1,5 +1,6 @@
 if not settings.startup["f_hd_a_sa_l_disable_turbo_transport_belt"].value then
-	data.raw["transport-belt"]["turbo-transport-belt"].belt_animation_set.animation_set = util.sprite_load("__factorio_hd_age_space_age_logistics__/data/space-age/graphics/entity/turbo-transport-belt/turbo-transport-belt", {
+	data.raw["transport-belt"]["turbo-transport-belt"].belt_animation_set.animation_set = util.sprite_load(
+	"__factorio_hd_age_space_age_logistics__/data/space-age/graphics/entity/turbo-transport-belt/turbo-transport-belt", {
 		priority = "extra-high",
 		size = 256,
 		scale = 0.25,
@@ -9,12 +10,14 @@ if not settings.startup["f_hd_a_sa_l_disable_turbo_transport_belt"].value then
 end
 
 if not settings.startup["f_hd_a_sa_l_disable_turbo_transport_belt_frozen_layer"].value then
-	data.raw["transport-belt"]["turbo-transport-belt"].belt_animation_set.frozen_patch = util.sprite_load("__factorio_hd_age_space_age_logistics__/data/space-age/graphics/entity/turbo-transport-belt/turbo-transport-belt-frozen", {
-		priority = "extra-high",
-		size = 256,
-		scale = 0.25,
-		line_length = 1,
-		lines_per_file = 20,
-		direction_count = 20
-	})
+	data.raw["transport-belt"]["turbo-transport-belt"].belt_animation_set.frozen_patch = util.sprite_load(
+	"__factorio_hd_age_space_age_logistics__/data/space-age/graphics/entity/turbo-transport-belt/turbo-transport-belt-frozen",
+		{
+			priority = "extra-high",
+			size = 256,
+			scale = 0.25,
+			line_length = 1,
+			lines_per_file = 20,
+			direction_count = 20
+		})
 end
