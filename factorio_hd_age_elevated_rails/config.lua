bdc0f0dfc60ec5b09e66008a682eccc442dafc8b
Ingo_Igel <Ingo_Igel@noreply.codeberg.org>
2025-12-10 12:08:34 +0100

    added new faster loading methode

--- factorio_hd_age_elevated_rails/config.lua
+++ factorio_hd_age_elevated_rails/config.lua
@@ -1,45 +1,44 @@
-local exclude = {	"*shadow*",
-					"*reflection*",
-					"*lights*",
-					"*ramp-segment-visualisations-middle*"
-				}
+local exclude = { "*shadow*",
+	"*reflection*",
+	"*lights*",
+	"*ramp-segment-visualisations-middle*"
+}
 
-local str= tostring(settings.startup["f_hd_a_er_disable_string"].value)
+local str = tostring(settings.startup["f_hd_a_er_disable_string"].value)
 if str then
-	for word in string.gmatch(str,'[^,%s]+') do
-		table.insert (exclude, "*"..word.."*")
+	for word in string.gmatch(str, '[^,%s]+') do
+		table.insert(exclude, "*" .. word .. "*")
 	end
 end
 
 if mods["factorio_hd_age_modpack"] or mods["factorio_hd_age_modpack_optional"] or mods["factorio_hd_age_modpack_base_game_only"] or mods["factorio_hd_age_modpack_base_game_optional"] then
-	local str1= tostring(settings.startup["f_hd_a_disable_string"].value)
+	local str1 = tostring(settings.startup["f_hd_a_disable_string"].value)
 	if str1 then
-		for word in string.gmatch(str1,'[^,%s]+') do
-			table.insert (exclude, "*"..word.."*")
+		for word in string.gmatch(str1, '[^,%s]+') do
+			table.insert(exclude, "*" .. word .. "*")
 		end
 	end
 end
 
 return {
+	scalefactor = 2,
 	resource_pack_name = "factorio_hd_age_elevated_rails",
+	exclude_names = exclude,
+	white_list = "__elevated-rails__/graphics/entity/",
 	data = {
-		__settings__ = {
-			exclude_names = exclude,
-			upscale = 2
-		},
-		["elevated-rails"]={
+		["elevated-rails"] = {
 			graphics = {
 				entity = {
-					["elevated-rail"]={
-						["elevated-rail-ending.png"]={},
-						["remnants"]={
-							["elevated-rail-remnants.png"]={}
+					["elevated-rail"] = {
+						["elevated-rail-ending.png"] = {},
+						["remnants"] = {
+							["elevated-rail-remnants.png"] = {}
 						}
 					},
-					["elevated-rail-chain-signal"]={},
-					["elevated-rail-pylon"]={ ["*"]={}, ["remnants"]={} },
-					["elevated-rail-ramp"]={ ["*"]={}, ["remnants"]={} },
-					["elevated-rail-signal"]={}
+					["elevated-rail-chain-signal"] = {},
+					["elevated-rail-pylon"] = { ["*"] = {}, ["remnants"] = {} },
+					["elevated-rail-ramp"] = { ["*"] = {}, ["remnants"] = {} },
+					["elevated-rail-signal"] = {}
 				}
 			}
 		}
