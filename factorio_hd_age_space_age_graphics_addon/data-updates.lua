e6978b731ce24668b11caa7ff514f4918aa71d4d
Ingo_Igel <Ingo_Igel@noreply.codeberg.org>
2026-01-10 11:28:59 +0100

    Fixed Starmap misalignment without PlanetsLib

--- factorio_hd_age_space_age_graphics_addon/data-updates.lua
+++ factorio_hd_age_space_age_graphics_addon/data-updates.lua
@@ -60,9 +60,9 @@ if mods["PlanetsLib"] then
 	})
 
 else
-	data.raw["utility-sprites"]["default"]["starmap_star"].layers[6].scale = 1
-	data.raw["utility-sprites"]["default"]["starmap_star"].layers[7].scale = 1.5
-	data.raw["utility-sprites"]["default"]["starmap_star"].layers[8] = {
+	data.raw["utility-sprites"]["default"]["starmap_star"].layers[19].scale = 1
+	data.raw["utility-sprites"]["default"]["starmap_star"].layers[20].scale = 1.5
+	data.raw["utility-sprites"]["default"]["starmap_star"].layers[21] = {
 		filename = "__factorio_hd_age_space_age_graphics_addon__/graphics/shattered-planet.png",
 		size = 960,
 		scale = 0.4,
