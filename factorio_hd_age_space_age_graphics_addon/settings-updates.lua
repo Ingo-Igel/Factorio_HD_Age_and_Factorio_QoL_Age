c1e8b6641925e50ce069781dd5dbd311a472c17e
Ingo_Igel <Ingo_Igel@noreply.codeberg.org>
2026-05-15 00:49:10 +0200

    Optimised settungs update

--- factorio_hd_age_space_age_graphics_addon/settings-updates.lua
+++ factorio_hd_age_space_age_graphics_addon/settings-updates.lua
@@ -1,69 +1,12 @@
-data:extend({
-    -- Runtime settings
-    {
-        type = "double-setting",
-        name = "visible-planets-planet-scale",
-        setting_type = "runtime-global",
-        default_value = 12.5,
-        minimum_value = 0.0,
-        order = "b[sprite]-b"
-    },
-    {
-        type = "int-setting",
-        name = "visible-planets-planet-pos-x",
-        setting_type = "runtime-global",
-        default_value = -108,
-        order = "b[sprite]-c"
-    },
-    {
-        type = "int-setting",
-        name = "visible-planets-planet-anim-dur",
-        setting_type = "runtime-global",
-        default_value = 500,
-        minimum_value = 1, -- Avoid division by zero
-        order = "b[sprite]-f"
-    },
-    {
-        type = "int-setting",
-        name = "visible-planets-planet-angle",
-        setting_type = "runtime-global",
-        default_value = 345,
-        minimum_value = 0,
-        maximum_value = 359,
-        order = "b[sprite]-g"
-    },
-    {
-        type = "bool-setting",
-        name = "visible-planets-enable-parallax",
-        setting_type = "runtime-global",
-        default_value = false,
-        order = "c[parallax]-a"
-    },
-    {
-        type = "double-setting",
-        name = "visible-planets-parallax-factor",
-        setting_type = "runtime-global",
-        default_value = 1,
-        minimum_value = 0.1,
-        order = "c[parallax]-b"
-    },
-    {
-        type = "bool-setting",
-        name = "visible-planets-enable-rotation",
-        setting_type = "runtime-global",
-        default_value = true,
-        order = "d[rotation]-a"
-    },
-    {
-        type = "double-setting",
-        name = "visible-planets-rotation-speed",
-        setting_type = "runtime-global",
-        default_value = 0.003,
-        minimum_value = -180,
-        maximum_value = 180,
-        order = "d[rotation]-b"
-    }
-})
+data.raw["double-setting"]["visible-planets-planet-scale"].default_value = 12.5
+data.raw["int-setting"]["visible-planets-planet-pos-x"].default_value = -108
+data.raw["int-setting"]["visible-planets-planet-anim-dur"].default_value = 500
+data.raw["int-setting"]["visible-planets-planet-angle"].default_value = 345
+data.raw["bool-setting"]["visible-planets-enable-parallax"].default_value = false
+data.raw["double-setting"]["visible-planets-parallax-factor"].default_value = 1
+data.raw["bool-setting"]["visible-planets-enable-rotation"].default_value = true
+data.raw["double-setting"]["visible-planets-rotation-speed"].default_value = 0.003
+
 data.raw["bool-setting"]["universe-widget-enabled"].default_value = false
 data.raw["bool-setting"]["asteroid-belt-1-enabled"].default_value = true
 data.raw["bool-setting"]["asteroid-belt-2-enabled"].default_value = true
