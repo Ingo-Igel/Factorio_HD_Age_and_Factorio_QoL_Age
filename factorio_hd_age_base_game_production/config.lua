b5201b900cea81416861e521dafb0bc138ba9484
Ingo_Igel <Ingo_Igel@noreply.codeberg.org>
2026-05-16 12:58:14 +0200

    Optimized string settings insertion

--- factorio_hd_age_base_game_production/config.lua
+++ factorio_hd_age_base_game_production/config.lua
@@ -8,21 +8,24 @@ local exclude = {
 	"*furnace-glow*"
 }
 
-local str = tostring(settings.startup["f_hd_a_bg_p_disable_string"].value)
-if str then
-	for word in string.gmatch(str, '[^,%s]+') do
-		table.insert(exclude, "*" .. word .. "*")
-	end
+local function add_disable_words(setting_name)
+    local setting = settings.startup[setting_name]
+    if not setting then
+        return
+    end
+
+    local value = setting.value
+    if value == "" then
+        return
+    end
+
+    for word in string.gmatch(value, "[^,%s]+") do
+        table.insert(exclude, "*" .. word .. "*")
+    end
 end
 
-if mods["factorio_hd_age_modpack"] or mods["factorio_hd_age_modpack_optional"] or mods["factorio_hd_age_modpack_base_game_only"] or mods["factorio_hd_age_modpack_base_game_optional"] then
-	local str1 = tostring(settings.startup["f_hd_a_disable_string"].value)
-	if str1 then
-		for word in string.gmatch(str1, '[^,%s]+') do
-			table.insert(exclude, "*" .. word .. "*")
-		end
-	end
-end
+add_disable_words("f_hd_a_bg_p_disable_string")
+add_disable_words("f_hd_a_disable_string")
 
 return {
 	scalefactor = 2,
