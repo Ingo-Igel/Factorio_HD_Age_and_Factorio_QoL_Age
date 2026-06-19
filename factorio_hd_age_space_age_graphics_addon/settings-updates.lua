5a6c204ecb68a4d0d8de16de032cefd764984818
Ingo_Igel <Ingo_Igel@noreply.codeberg.org>
2026-06-19 19:50:31 +0200

    removed "visible-planets" mod

--- factorio_hd_age_space_age_graphics_addon/settings-updates.lua
+++ factorio_hd_age_space_age_graphics_addon/settings-updates.lua
@@ -1,12 +1,3 @@
-data.raw["double-setting"]["visible-planets-planet-scale"].default_value = 12.5
-data.raw["int-setting"]["visible-planets-planet-pos-x"].default_value = -108
-data.raw["int-setting"]["visible-planets-planet-anim-dur"].default_value = 500
-data.raw["int-setting"]["visible-planets-planet-angle"].default_value = 345
-data.raw["bool-setting"]["visible-planets-enable-parallax"].default_value = false
-data.raw["double-setting"]["visible-planets-parallax-factor"].default_value = 1
-data.raw["bool-setting"]["visible-planets-enable-rotation"].default_value = true
-data.raw["double-setting"]["visible-planets-rotation-speed"].default_value = 0.003
-
 data.raw["bool-setting"]["universe-widget-enabled"].default_value = false
 data.raw["bool-setting"]["asteroid-belt-1-enabled"].default_value = true
 data.raw["bool-setting"]["asteroid-belt-2-enabled"].default_value = true
