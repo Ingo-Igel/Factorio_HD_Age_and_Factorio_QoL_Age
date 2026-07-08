f4a7c658a49d73be7ef373739e1bb3ea81c1727e
Ingo_Igel <Ingo_Igel@noreply.codeberg.org>
2026-07-09 00:16:38 +0200

    code cleanup

--- factorio_hd_age_space_age_decorative/prototypes/sunken-ruin.lua
+++ factorio_hd_age_space_age_decorative/prototypes/sunken-ruin.lua
@@ -3,6 +3,6 @@ data.raw["simple-entity"]["fulgora-sunk-ruin-big"].pictures.layers[1] =
 		"__factorio_hd_age_space_age_decorative__/data/space-age/graphics/decorative/fulgora-ruin-flooded/big/sunken-ruin-big",
 		{
 			scale = 0.25,
-			variation_count = 7,
+			variation_count = 7
 		}
 	)
