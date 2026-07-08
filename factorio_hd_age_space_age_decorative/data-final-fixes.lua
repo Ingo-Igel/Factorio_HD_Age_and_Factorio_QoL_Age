043e415d01c9e4eab25a2392ae1df699a415fa4f
Ingo_Igel <Ingo_Igel@noreply.codeberg.org>
2026-07-09 00:09:38 +0200

    fixed settings name

--- factorio_hd_age_space_age_decorative/data-final-fixes.lua
+++ factorio_hd_age_space_age_decorative/data-final-fixes.lua
@@ -7,7 +7,7 @@ end
 if not settings.startup["f_hd_a_sa_d_disable_vulcanus_chimneys"].value then
 	require("prototypes.vulcanus-chimney")
 end
-if not settings.startup["f_hd_a_sa_d_disable_vulcanus_rocks"].value then
+if not settings.startup["f_hd_a_sa_b_disable_vulcanus_rocks"].value then
 	require("prototypes.vulcanus-rocks")
 end
 
