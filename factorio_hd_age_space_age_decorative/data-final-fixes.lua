f1e2463d7991d6e0d183c1a789f71dc391532822
Ingo_Igel <Ingo_Igel@noreply.codeberg.org>
2026-07-08 19:42:12 +0200

    added new settings

--- factorio_hd_age_space_age_decorative/data-final-fixes.lua
+++ factorio_hd_age_space_age_decorative/data-final-fixes.lua
@@ -1,8 +1,14 @@
-require("prototypes.demolisher-corpse")
-require("prototypes.sunken-ruin")
+if not settings.startup["f_hd_a_sa_d_disable_vulcanus_demolisher-corpse"].value then
+	require("prototypes.demolisher-corpse")
+end
+if not settings.startup["f_hd_a_sa_d_disable_fulgora_sunken-ruin"].value then
+	require("prototypes.sunken-ruin")
+end
 if not settings.startup["f_hd_a_sa_d_disable_vulcanus_chimneys"].value then
 	require("prototypes.vulcanus-chimney")
 end
-require("prototypes.vulcanus-rocks")
+if not settings.startup["f_hd_a_sa_d_disable_vulcanus_rocks"].value then
+	require("prototypes.vulcanus-rocks")
+end
 
 require("texture-loading")
--- factorio_hd_age_space_age_decorative/settings.lua
+++ factorio_hd_age_space_age_decorative/settings.lua
@@ -2,10 +2,34 @@ data:extend {
 	{
 		type = "bool-setting",
 		setting_type = "startup",
-		name = "f_hd_a_sa_d_disable_vulcanus_chimneys",
+		name = "f_hd_a_sa_d_disable_vulcanus_demolisher-corpse",
 		default_value = false,
 		forced_value = true,
 		order = "a"
+	},
+	{
+		type = "bool-setting",
+		setting_type = "startup",
+		name = "f_hd_a_sa_d_disable_fulgora_sunken-ruin",
+		default_value = false,
+		forced_value = true,
+		order = "b"
+	},
+	{
+		type = "bool-setting",
+		setting_type = "startup",
+		name = "f_hd_a_sa_d_disable_vulcanus_chimneys",
+		default_value = false,
+		forced_value = true,
+		order = "c"
+	},
+	{
+		type = "bool-setting",
+		setting_type = "startup",
+		name = "f_hd_a_sa_d_disable_vulcanus_rocks",
+		default_value = false,
+		forced_value = true,
+		order = "d"
 	},
 	{
 		type = "string-setting",
