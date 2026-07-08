95a0ea4962a3b6371d57f42229987b3c4da099e2
Ingo_Igel <Ingo_Igel@noreply.codeberg.org>
2026-07-08 19:40:47 +0200

    added demolischer corpse

---
+++ factorio_hd_age_space_age_decorative/prototypes/demolisher-corpse.lua
@@ -0,0 +1,29 @@
+data.raw["simple-entity"]["small-demolisher-corpse"].pictures.layers[1] =
+	util.sprite_load(
+		"__factorio_hd_age_space_age_decorative__/data/space-age/graphics/decorative/demolisher-corpse/demolisher-corpse",
+		{
+			scale = 0.25,
+			variation_count = 15,
+			multiply_shift = 1
+		}
+	)
+
+data.raw["simple-entity"]["medium-demolisher-corpse"].pictures.layers[1] =
+	util.sprite_load(
+		"__factorio_hd_age_space_age_decorative__/data/space-age/graphics/decorative/demolisher-corpse/demolisher-corpse",
+		{
+			scale = 0.5 * 0.75,
+			variation_count = 15,
+			multiply_shift = 1
+		}
+	)
+
+data.raw["simple-entity"]["big-demolisher-corpse"].pictures.layers[1] =
+	util.sprite_load(
+		"__factorio_hd_age_space_age_decorative__/data/space-age/graphics/decorative/demolisher-corpse/demolisher-corpse",
+		{
+			scale = 0.5,
+			variation_count = 15,
+			multiply_shift = 1
+		}
+	)
