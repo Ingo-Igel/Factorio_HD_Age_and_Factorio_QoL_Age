598b37a62254eb467ad8499c0d0b621707f7a76c
Ingo_Igel <Ingo_Igel@noreply.codeberg.org>
2026-07-10 10:50:28 +0200

    optimised transition loading

--- factorio_hd_age_space_age_terrain_fulgora/prototypes/terrain-fulgora.lua
+++ factorio_hd_age_space_age_terrain_fulgora/prototypes/terrain-fulgora.lua
@@ -1,3 +1,6 @@
+local disable_water_transitions = settings.startup["f_hd_a_sa_tf_disable_water-transitions"].value
+local disable_out_of_map_transitions = settings.startup["f_hd_a_sa_tf_disable_out-of-map-transitions"].value
+
 local function tile_variations_template_hd(high_res_picture, high_res_transition_mask, options)
 	local function main_variation_hd(size_)
 		local y_ = ((size_ == 1) and 0) or ((size_ == 2) and 256) or ((size_ == 4) and 640) or 1280
@@ -162,14 +165,6 @@ tile_spritesheet_layout_hd.transition_4_4_8_1_1 =
 	background               = { x_offset = 1088 * 2 }
 }
 
-tile_spritesheet_layout_hd.simple_white_mask = {
-	spritesheet = "__core__/graphics/white-square.png",
-	x = 0,
-	y = 0,
-	count = 1,
-	scale = 32
-}
-
 local patch_for_inner_corner_of_transition_between_transition = {
 	filename =
 	"__factorio_hd_age_space_age_terrain_fulgora__/data/base/graphics/terrain/water-transitions/water-patch.png",
@@ -178,282 +173,93 @@ local patch_for_inner_corner_of_transition_between_transition = {
 	height = 64 * 2
 }
 
-local fulgora_rock_sand_transitions = {
-	{
-		to_tiles = water_tile_type_names,
-		transition_group = water_transition_group_id,
-		background_layer_group = "water",
-		background_layer_offset = -5,
-		masked_background_layer_offset = 1,
-		offset_background_layer_by_tile_layer = false,
-		spritesheet =
-		"__factorio_hd_age_space_age_terrain_fulgora__/data/space-age/graphics/terrain/water-transitions/fulgora-rock-slab-transition.png",
-		layout = tile_spritesheet_layout_hd.transition_16_16_16_4_8_short,
-		background_enabled = false,
-		effect_map_layout = {
-			spritesheet =
-			"__factorio_hd_age_space_age_terrain_fulgora__/data/space-age/graphics/terrain/effect-maps/water-fulgora-sand-mask.png",
-			--tile_height = 2,
-			inner_corner_tile_height = 2,
-			outer_corner_tile_height = 2,
-			side_tile_height = 2,
-			u_transition_tile_height = 2,
-			o_transition_count = 1
+local base = "__factorio_hd_age_space_age_terrain_fulgora__/data/space-age/graphics/terrain/"
+
+local function load_sand_transition(name, variant)
+	local assets = {
+		rock = {
+			water_transition = "water-transitions/fulgora-rock-slab-transition.png",
+			water_between = "water-transitions/fulgora-sand-transition.png",
+			out_of_map = "out-of-map-transition/fulgora-out-of-map-transition-b.png",
 		},
-		background_mask_layout = tile_spritesheet_layout_hd.simple_white_mask
-	},
-	{
-		to_tiles = out_of_map_tile_type_names,
-		transition_group = out_of_map_transition_group_id,
-		background_layer_offset = 1,
-		background_layer_group = "zero",
-		offset_background_layer_by_tile_layer = true,
-		spritesheet =
-		"__factorio_hd_age_space_age_terrain_fulgora__/data/space-age/graphics/terrain/out-of-map-transition/fulgora-out-of-map-transition.png",
-		layout = tile_spritesheet_layout_hd.transition_4_4_8_1_1,
-		overlay_enabled = false
+		oil = {
+			water_transition = "water-transitions/fulgora-oil-sand.png",
+			water_between = "water-transitions/fulgora-oil-sand-transition.png",
+			out_of_map = "out-of-map-transition/oil-out-of-map-transition.png",
+		}
 	}
-}
 
-local fulgora_oil_transitions = {
-	{
-		to_tiles = water_tile_type_names,
-		transition_group = water_transition_group_id,
+	if not disable_water_transitions then
+		data.raw.tile[name].transitions[1].spritesheet = base .. assets[variant].water_transition
+		data.raw.tile[name].transitions[1].layout = tile_spritesheet_layout_hd.transition_16_16_16_4_8_short
+		data.raw.tile[name].transitions[1].effect_map_layout.spritesheet = base .. "effect-maps/water-fulgora-sand-mask.png"
 
-		background_layer_group = "water",
-		background_layer_offset = -5,
-		masked_background_layer_offset = 1,
-		offset_background_layer_by_tile_layer = false,
-		spritesheet =
-		"__factorio_hd_age_space_age_terrain_fulgora__/data/space-age/graphics/terrain/water-transitions/fulgora-oil-sand.png",
-		layout = tile_spritesheet_layout_hd.transition_16_16_16_4_8_short,
-		background_enabled = false,
-		effect_map_layout = {
-			spritesheet =
-			"__factorio_hd_age_space_age_terrain_fulgora__/data/space-age/graphics/terrain/effect-maps/water-fulgora-sand-mask.png",
-			inner_corner_tile_height = 2,
-			outer_corner_tile_height = 2,
-			side_tile_height = 2,
-			u_transition_tile_height = 2,
-			o_transition_count = 1
-		},
-		background_mask_layout = tile_spritesheet_layout_hd.simple_white_mask
-	},
-	{
-		to_tiles = out_of_map_tile_type_names,
-		transition_group = out_of_map_transition_group_id,
-		background_layer_offset = 1,
-		background_layer_group = "zero",
-		offset_background_layer_by_tile_layer = true,
-		spritesheet =
-		"__factorio_hd_age_space_age_terrain_fulgora__/data/space-age/graphics/terrain/out-of-map-transition/oil-out-of-map-transition.png",
-		layout = tile_spritesheet_layout_hd.transition_4_4_8_1_1,
-		overlay_enabled = false
-	}
-}
-local fulgora_oil_transitions_between_transitions = {
-	{
-		transition_group1 = water_transition_group_id,
-		transition_group2 = out_of_map_transition_group_id,
-		background_layer_offset = 1,
-		background_layer_group = "zero",
-		offset_background_layer_by_tile_layer = true,
-		spritesheet =
-		"__factorio_hd_age_space_age_terrain_fulgora__/data/space-age/graphics/terrain/out-of-map-transition/oil-out-of-map-transition-b.png",
-		layout = tile_spritesheet_layout_hd.transition_3_3_3_1_0,
-		background_enabled = true,
-		background_mask_layout = tile_spritesheet_layout_hd.simple_3_3_3_1_0_three_way_edge_mask,
-	}
-}
+		data.raw.tile[name].transitions_between_transitions[1].spritesheet = base .. assets[variant].water_between
+		data.raw.tile[name].transitions_between_transitions[1].layout = tile_spritesheet_layout_hd.transition_3_3_3_1_0_only_u_tall
+		data.raw.tile[name].transitions_between_transitions[1].effect_map_layout.spritesheet = base .. "effect-maps/water-fulgora-sand-to-land-mask.png"
+		data.raw.tile[name].transitions_between_transitions[1].water_patch = patch_for_inner_corner_of_transition_between_transition
+	end
 
-local fulgora_sand_transitions_between_transitions = {
-	{
-		transition_group1 = default_transition_group_id,
-		transition_group2 = water_transition_group_id,
-		spritesheet =
-		"__factorio_hd_age_space_age_terrain_fulgora__/data/space-age/graphics/terrain/water-transitions/fulgora-sand-transition.png",
-		layout = tile_spritesheet_layout_hd.transition_3_3_3_1_0_only_u_tall,
-		background_enabled = false,
-		effect_map_layout = {
-			spritesheet =
-			"__factorio_hd_age_space_age_terrain_fulgora__/data/space-age/graphics/terrain/effect-maps/water-fulgora-sand-to-land-mask.png",
-			inner_corner_tile_height = 2,
-			outer_corner_tile_height = 2,
-			side_tile_height = 2,
-			o_transition_count = 0
-		},
-		water_patch = patch_for_inner_corner_of_transition_between_transition,
-	},
-	{
-		transition_group1 = default_transition_group_id,
-		transition_group2 = out_of_map_transition_group_id,
-		background_layer_offset = 1,
-		background_layer_group = "zero",
-		offset_background_layer_by_tile_layer = true,
-		spritesheet =
-		"__factorio_hd_age_space_age_terrain_fulgora__/data/space-age/graphics/terrain/out-of-map-transition/fulgora-out-of-map-transition-b.png",
-		layout = tile_spritesheet_layout_hd.transition_3_3_3_1_0,
-		overlay_enabled = false
-	},
-	{
-		transition_group1 = water_transition_group_id,
-		transition_group2 = out_of_map_transition_group_id,
-		background_layer_group = "water",
-		background_layer_offset = -5,
-		masked_background_layer_offset = 1,
-		offset_background_layer_by_tile_layer = false,
-		spritesheet =
-		"__factorio_hd_age_space_age_terrain_fulgora__/data/space-age/graphics/terrain/out-of-map-transition/fulgora-shore-out-of-map-transition.png",
-		layout = tile_spritesheet_layout_hd.transition_3_3_3_1_0,
-		effect_map_layout = {
-			spritesheet =
-			"__factorio_hd_age_space_age_terrain_fulgora__/data/space-age/graphics/terrain/effect-maps/water-fulgora-sand-to-out-of-map-mask.png",
-			o_transition_count = 0
-		},
-		background_mask_layout = tile_spritesheet_layout_hd.simple_3_3_3_1_0_three_way_edge_mask,
-		water_patch = patch_for_inner_corner_of_transition_between_transition
-	}
-}
+	if not disable_out_of_map_transitions then
+		data.raw.tile[name].transitions[2].spritesheet = base .. assets[variant].out_of_map
+		data.raw.tile[name].transitions[2].layout =
+			tile_spritesheet_layout_hd.transition_4_4_8_1_1
 
-local fulgora_oil_sand_transitions = {
-	{
-		to_tiles = water_tile_type_names,
-		transition_group = water_transition_group_id,
-		background_layer_group = "water",
-		background_layer_offset = -5,
-		masked_background_layer_offset = 1,
-		offset_background_layer_by_tile_layer = false,
-		spritesheet =
-		"__factorio_hd_age_space_age_terrain_fulgora__/data/space-age/graphics/terrain/water-transitions/fulgora-oil-sand.png",
-		layout = tile_spritesheet_layout_hd.transition_16_16_16_4_8_short,
-		background_enabled = false,
-		effect_map_layout = {
-			spritesheet =
-			"__factorio_hd_age_space_age_terrain_fulgora__/data/space-age/graphics/terrain/effect-maps/water-fulgora-sand-mask.png",
-			inner_corner_tile_height = 2,
-			outer_corner_tile_height = 2,
-			side_tile_height = 2,
-			u_transition_tile_height = 2,
-			o_transition_count = 1
-		},
-		background_mask_layout = tile_spritesheet_layout_hd.simple_white_mask
-	},
-	{
-		to_tiles = out_of_map_tile_type_names,
-		transition_group = out_of_map_transition_group_id,
-		background_layer_offset = 1,
-		background_layer_group = "zero",
-		offset_background_layer_by_tile_layer = true,
-		spritesheet =
-		"__factorio_hd_age_space_age_terrain_fulgora__/data/space-age/graphics/terrain/out-of-map-transition/oil-out-of-map-transition.png",
-		layout = tile_spritesheet_layout_hd.transition_4_4_8_1_1,
-		overlay_enabled = false
-	}
-}
-local fulgora_oil_sand_transitions_between_transitions = {
-	{
-		transition_group1 = default_transition_group_id,
-		transition_group2 = water_transition_group_id,
-		spritesheet =
-		"__factorio_hd_age_space_age_terrain_fulgora__/data/space-age/graphics/terrain/water-transitions/fulgora-oil-sand-transition.png",
-		layout = tile_spritesheet_layout_hd.transition_3_3_3_1_0_only_u_tall,
-		background_enabled = false,
-		effect_map_layout = {
-			spritesheet =
-			"__factorio_hd_age_space_age_terrain_fulgora__/data/space-age/graphics/terrain/effect-maps/water-fulgora-sand-to-land-mask.png",
-			inner_corner_tile_height = 2,
-			outer_corner_tile_height = 2,
-			side_tile_height = 2,
-			o_transition_count = 0
-		},
-		water_patch = patch_for_inner_corner_of_transition_between_transition,
-	},
-	{
-		transition_group1 = default_transition_group_id,
-		transition_group2 = out_of_map_transition_group_id,
-		background_layer_offset = 1,
-		background_layer_group = "zero",
-		offset_background_layer_by_tile_layer = true,
-		spritesheet =
-		"__factorio_hd_age_space_age_terrain_fulgora__/data/space-age/graphics/terrain/out-of-map-transition/fulgora-out-of-map-transition.png",
-		layout = tile_spritesheet_layout_hd.transition_3_3_3_1_0,
-		overlay_enabled = false
-	},
-	{
-		transition_group1 = water_transition_group_id,
-		transition_group2 = out_of_map_transition_group_id,
-		background_layer_group = "water",
-		background_layer_offset = -5,
-		masked_background_layer_offset = 1,
-		offset_background_layer_by_tile_layer = false,
-		spritesheet =
-		"__factorio_hd_age_space_age_terrain_fulgora__/data/space-age/graphics/terrain/out-of-map-transition/fulgora-shore-out-of-map-transition.png",
-		layout = tile_spritesheet_layout_hd.transition_3_3_3_1_0,
-		effect_map_layout = {
-			spritesheet =
-			"__factorio_hd_age_space_age_terrain_fulgora__/data/space-age/graphics/terrain/effect-maps/water-fulgora-sand-to-out-of-map-mask.png",
-			o_transition_count = 0
-		},
-		background_mask_layout = tile_spritesheet_layout_hd.simple_white_mask,
-		water_patch = patch_for_inner_corner_of_transition_between_transition
-	}
-}
+		data.raw.tile[name].transitions_between_transitions[2].spritesheet = base .. "out-of-map-transition/fulgora-out-of-map-transition.png"
+		data.raw.tile[name].transitions_between_transitions[2].layout = tile_spritesheet_layout_hd.transition_3_3_3_1_0
 
-if not settings.startup["f_hd_a_sa_tf_disable_fulgora_transitions"].value then
-	data.raw.tile["fulgoran-dust"].transitions = fulgora_rock_sand_transitions
-	data.raw.tile["fulgoran-dust"].transitions_between_transitions = fulgora_sand_transitions_between_transitions
-
-	data.raw.tile["fulgoran-dunes"].transitions = fulgora_rock_sand_transitions
-	data.raw.tile["fulgoran-dunes"].transitions_between_transitions = fulgora_sand_transitions_between_transitions
-
-	data.raw.tile["fulgoran-sand"].transitions = fulgora_rock_sand_transitions
-	data.raw.tile["fulgoran-sand"].transitions_between_transitions = fulgora_sand_transitions_between_transitions
-
-	data.raw.tile["fulgoran-rock"].transitions = fulgora_rock_sand_transitions
-	data.raw.tile["fulgoran-rock"].transitions_between_transitions = fulgora_sand_transitions_between_transitions
-
-	data.raw.tile["fulgoran-paving"].transitions = fulgora_rock_sand_transitions
-	data.raw.tile["fulgoran-paving"].transitions_between_transitions = fulgora_sand_transitions_between_transitions
-
-	data.raw.tile["fulgoran-walls"].transitions = fulgora_rock_sand_transitions
-	data.raw.tile["fulgoran-walls"].transitions_between_transitions = fulgora_sand_transitions_between_transitions
-
-	data.raw.tile["fulgoran-conduit"].transitions = fulgora_rock_sand_transitions
-	data.raw.tile["fulgoran-conduit"].transitions_between_transitions = fulgora_sand_transitions_between_transitions
-
-	data.raw.tile["fulgoran-machinery"].transitions = fulgora_rock_sand_transitions
-	data.raw.tile["fulgoran-machinery"].transitions_between_transitions = fulgora_sand_transitions_between_transitions
-
-	data.raw.tile["oil-ocean-shallow"].transitions = fulgora_oil_sand_transitions
-	data.raw.tile["oil-ocean-shallow"].transitions_between_transitions = fulgora_oil_sand_transitions_between_transitions
-
-	data.raw.tile["oil-ocean-shallow-2"].transitions = fulgora_oil_sand_transitions
-	data.raw.tile["oil-ocean-shallow-2"].transitions_between_transitions = fulgora_oil_sand_transitions_between_transitions
+		data.raw.tile[name].transitions_between_transitions[3].spritesheet = base .. "out-of-map-transition/fulgora-shore-out-of-map-transition.png"
+		data.raw.tile[name].transitions_between_transitions[3].layout = tile_spritesheet_layout_hd.transition_3_3_3_1_0
+		data.raw.tile[name].transitions_between_transitions[3].effect_map_layout.spritesheet = base .. "effect-maps/water-fulgora-sand-to-out-of-map-mask.png"
+		data.raw.tile[name].transitions_between_transitions[3].water_patch = patch_for_inner_corner_of_transition_between_transition
+	end
 end
 
-if not settings.startup["f_hd_a_sa_tf_disable_oil_ocean_deep"].value then
-	data.raw.tile["oil-ocean-deep"].transitions = fulgora_oil_transitions
-	data.raw.tile["oil-ocean-deep"].transitions_between_transitions = fulgora_oil_transitions_between_transitions
+for _, name in ipairs({
+    "fulgoran-dust",
+    "fulgoran-dunes",
+    "fulgoran-sand",
+    "fulgoran-rock",
+    "fulgoran-paving",
+    "fulgoran-walls",
+    "fulgoran-conduit",
+    "fulgoran-machinery"
+}) do
+    load_sand_transition(name, "rock")
+end
 
-	data.raw.tile["oil-ocean-deep-2"].transitions = table.deepcopy(data.raw.tile["water-shallow"].transitions)
-	data.raw.tile["oil-ocean-deep-2"].transitions_between_transitions = table.deepcopy(data.raw.tile["water-shallow"].transitions_between_transitions)
+load_sand_transition("oil-ocean-shallow", "oil")
+load_sand_transition("oil-ocean-shallow-2", "oil")
 
-	data.raw.tile["oil-ocean-deep-2"].transitions[1].spritesheet = "__factorio_hd_age_space_age_terrain_fulgora__/data/space-age/graphics/terrain/out-of-map-transition/oil-out-of-map-transition.png"
-	data.raw.tile["oil-ocean-deep-2"].transitions[1].layout = tile_spritesheet_layout_hd.transition_4_4_8_1_1
+local function load_oil_ocean(name, number)
+	if not disable_water_transitions and (name == "oil-ocean-deep") then
+		data.raw.tile[name].transitions[1].spritesheet = base .. "water-transitions/fulgora-oil-sand.png"
+		data.raw.tile[name].transitions[1].layout = tile_spritesheet_layout_hd.transition_16_16_16_4_8_short
+		data.raw.tile[name].transitions[1].effect_map_layout.spritesheet = base .. "effect-maps/water-fulgora-sand-mask.png"
+	end
 
-	data.raw.tile["oil-ocean-deep-2"].transitions_between_transitions[1].spritesheet = "__factorio_hd_age_space_age_terrain_fulgora__/data/space-age/graphics/terrain/out-of-map-transition/oil-out-of-map-transition-b.png"
-	data.raw.tile["oil-ocean-deep-2"].transitions_between_transitions[1].layout = tile_spritesheet_layout_hd.transition_3_3_3_1_0
+	if not disable_out_of_map_transitions then
+		data.raw.tile[name].transitions[number].spritesheet = base .. "out-of-map-transition/oil-out-of-map-transition.png"
+		data.raw.tile[name].transitions[number].layout = tile_spritesheet_layout_hd.transition_4_4_8_1_1
 
-	local variants = tile_variations_template_with_transitions_hd(
-		"__factorio_hd_age_space_age_terrain_fulgora__/data/space-age/graphics/terrain/oil-ocean-deep.png",
+		data.raw.tile[name].transitions_between_transitions[1].spritesheet = base .. "out-of-map-transition/oil-out-of-map-transition-b.png"
+		data.raw.tile[name].transitions_between_transitions[1].layout = tile_spritesheet_layout_hd.transition_3_3_3_1_0
+	end
+
+	data.raw.tile[name].variants = tile_variations_template_with_transitions_hd(
+		base .. "oil-ocean-deep.png",
 		{
 			max_size = 4,
 			[1] = { weights = { 0.085, 0.085, 0.085, 0.085, 0.087, 0.085, 0.065, 0.085, 0.045, 0.045, 0.045, 0.045, 0.005, 0.025, 0.045, 0.045 } },
 			[2] = { probability = 1, weights = { 0.018, 0.020, 0.015, 0.025, 0.015, 0.020, 0.025, 0.015, 0.025, 0.025, 0.010, 0.025, 0.020, 0.025, 0.025, 0.010 }, },
 			[4] = { probability = 0.1, weights = { 0.018, 0.020, 0.015, 0.025, 0.015, 0.020, 0.025, 0.015, 0.025, 0.025, 0.010, 0.025, 0.020, 0.025, 0.025, 0.010 }, },
-		}
-	)
+		})
+end
 
-	data.raw.tile["oil-ocean-deep"].variants = variants
-	data.raw.tile["oil-ocean-deep-2"].variants = variants
+if not settings.startup["f_hd_a_sa_tf_disable_oil_ocean_deep"].value then
+	data.raw.tile["oil-ocean-deep-2"].transitions = table.deepcopy(data.raw.tile["water-shallow"].transitions)
+	data.raw.tile["oil-ocean-deep-2"].transitions_between_transitions = table.deepcopy(data.raw.tile["water-shallow"].transitions_between_transitions)
+
+	load_oil_ocean("oil-ocean-deep", 2)
+	load_oil_ocean("oil-ocean-deep-2", 1)
 end
--- factorio_hd_age_space_age_terrain_fulgora/settings.lua
+++ factorio_hd_age_space_age_terrain_fulgora/settings.lua
@@ -2,7 +2,7 @@ data:extend {
 	{
 		type = "bool-setting",
 		setting_type = "startup",
-		name = "f_hd_a_sa_tf_disable_fulgora_transitions",
+		name = "f_hd_a_sa_tf_disable_oil_ocean_deep",
 		default_value = false,
 		forced_value = true,
 		order = "a"
@@ -10,10 +10,18 @@ data:extend {
 	{
 		type = "bool-setting",
 		setting_type = "startup",
-		name = "f_hd_a_sa_tf_disable_oil_ocean_deep",
+		name = "f_hd_a_sa_tf_disable_water-transitions",
 		default_value = false,
 		forced_value = true,
 		order = "b"
+	},
+	{
+		type = "bool-setting",
+		setting_type = "startup",
+		name = "f_hd_a_sa_tf_disable_out-of-map-transitions",
+		default_value = false,
+		forced_value = true,
+		order = "c"
 	},
 	{
 		type = "string-setting",
